d5cb93d98a005f6dad13ec0fd6579fcfb83020b7
asherban <asherban@yahoo.com>
2010-12-03 17:38:33 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReflectionsOnCodeBlog::Application.config.secret_token = '27f67885f9a2341be49f4f311186a27ddda481bbc8ca6e2753b24b1f8e781ce38b9aa426fd7b30ba19fd675a1ae77c6f9e7107279488761f5fc6cf4492613a39'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ReflectionsOnCodeBlog::Application.config.session_store :cookie_store, :key => '_reflections_on_code_blog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ReflectionsOnCodeBlog::Application.config.session_store :active_record_store
